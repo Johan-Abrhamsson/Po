761cfc016f7c3eaca252c3722775cac1f9c08f1c
Johan Abrahamsson <71386129+Johan-Abrhamsson@users.noreply.github.com>
2021-04-30 16:21:51 +0200

    fixed movement

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
-var speed = 5;
+var speed = 2;
 var posx = 50;
 var posy = 40;
 var lenght = 50;
 var scale = 2;
-window.onload = main;
-function main() {
+var timeMode = 1;
+
 let canvas = document.getElementById("canvas");
 const b = canvas.getContext("2d");
 
-if (b === null){
-    alert("unable to initialize 2d. Your browser may not support it.");
+function updateCanvas() {
+    b.clearRect(0, 0, 500, 500);
+    
+    b.lineWidth = 3*scale;
+    b.beginPath();
+    b.moveTo(50*scale, 20*scale);
+    b.lineTo(50*scale, 80*scale);
+    b.moveTo(50*scale, 50*scale);
+    b.lineTo(150*scale,50*scale);
+    b.moveTo(100*scale,30*scale);
+    b.lineTo(100*scale,70*scale);
+    b.moveTo(150*scale,20*scale);
+    b.lineTo(150*scale,80*scale);
+    b.closePath();
+    b.stroke();
+    b.strokeRect(posx*scale, posy*scale, 20*scale,20*scale);
+    switch(timeMode)
+    {
+        case 0:
+    if (posx*scale < 50*scale){
+        speed=speed*-1;
+    }
+    else if (posx*scale > 130*scale){
+        speed=speed*-1;
+    }
+    break;
+
+    case 1:
+      if (posx*scale < 50*scale){
+        speed=speed*-1;
+        posx=posx+length;
+    }
+    else if (posx*scale > 130*scale){
+        speed=speed*-1;
+        posx=posx-length;
+    }
+    break;  
+    }
+
+    posx=posx+speed;
+    requestAnimationFrame(updateCanvas);
+
 }
 
-b.lineWidth = 3*scale;
-b.beginPath();
-b.moveTo(50*scale, 20*scale);
-b.lineTo(50*scale, 80*scale);
-b.moveTo(50*scale, 50*scale);
-b.lineTo(150*scale,50*scale);
-b.moveTo(100*scale,30*scale);
-b.lineTo(100*scale,70*scale);
-b.moveTo(150*scale,20*scale);
-b.lineTo(150*scale,80*scale);
-b.closePath();
-b.stroke();
-b.strokeRect(posx*scale, posy*scale, 20*scale,20*scale);
-if (posy*scale<=50*scale){
-    speed=speed;
-}
-else if (posy*scale>=150*scale){
-    speed=-speed;
-}
-posy=posy+speed
-Node.appendChild(strokeRect)
-b.clearColor(0.0, 0.0, 0.0, 1.0);
-b.clear(b.COLOR_BUFFER_BIT);
-}
+
+/*
+
+velocity = 1.0 * acceleration * deltaTime
+position = position + velocity * deltaTime
+
+*/
+
+updateCanvas();
